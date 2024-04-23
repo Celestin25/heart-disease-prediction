b1ffaeadeadd2bc6164086866fdd141bde1ae4e5
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2024-04-23 16:19:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,87 +3,53 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-
 st.set_page_config(page_title="Health Assistant",
                    layout="wide",
                    page_icon="🧑‍⚕️")
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
 
 with st.sidebar:
-    selected = option_menu('Multiple Disease Prediction System',
-
-                           [
-                            'Heart Disease Prediction'
-                            ],
+    selected = option_menu('Disease Prediction System',
+                           ['Heart Disease Prediction'],
                            menu_icon='hospital-fill',
                            icons=['heart'],
                            default_index=0)
 
-# Heart Disease Prediction Page
 if selected == 'Heart Disease Prediction':
-
-    # page title
     st.title('Heart Disease Prediction using ML')
+    st.markdown("Please fill out the following details to predict the presence of heart disease.")
 
+   
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        age = st.text_input('Age')
+        age = st.number_input('Age', min_value=1, max_value=120, help='Enter your age in years.')
+        trestbps = st.number_input('Resting Blood Pressure (mm Hg)', min_value=50, max_value=300, help='Normal range: 90-120')
+        restecg = st.selectbox('Resting Electrocardiographic Results', options=[0, 1, 2], format_func=lambda x: {0: "Normal", 1: "ST-T Wave Abnormality", 2: "Probable or Definite Left Ventricular Hypertrophy"}[x], help="0: Normal, 1: ST-T wave abnormality, 2: Probable LVH")
+        oldpeak = st.number_input('ST Depression Induced by Exercise', min_value=0.0, max_value=10.0, step=0.1, help='Depression amount measured in depression')
+        ca = st.number_input('Major Vessels Colored by Flouroscopy', min_value=0, max_value=4, help='Number of major vessels colored by flouroscopy')
 
     with col2:
-        sex = st.text_input('Sex')
+        sex = st.selectbox('Sex', options=[0, 1], format_func=lambda x: {0: "Female", 1: "Male"}[x], help='0: Female, 1: Male')
+        chol = st.number_input('Serum Cholestoral (mg/dl)', min_value=100, max_value=700, help='Normal range: 125-200 mg/dL')
+        thalach = st.number_input('Maximum Heart Rate Achieved', min_value=60, max_value=250, help='Enter the max heart rate achieved during exercise')
+        slope = st.selectbox('Slope of the Peak Exercise ST Segment', options=[0, 1, 2], format_func=lambda x: {0: "Upsloping", 1: "Flat", 2: "Downsloping"}[x], help="0: Upsloping, 1: Flat, 2: Downsloping")
 
     with col3:
-        cp = st.text_input('Chest Pain types')
-
-    with col1:
-        trestbps = st.text_input('Resting Blood Pressure')
-
-    with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl')
-
-    with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
-
-    with col1:
-        restecg = st.text_input('Resting Electrocardiographic results')
-
-    with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved')
-
-    with col3:
-        exang = st.text_input('Exercise Induced Angina')
-
-    with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
-
-    with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment')
-
-    with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
-
-    with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
-
-    # code for Prediction
-    heart_diagnosis = ''
-
-    # creating a button for Prediction
+        cp = st.selectbox('Chest Pain Type', options=[0, 1, 2, 3], format_func=lambda x: {0: "Typical Angina", 1: "Atypical Angina", 2: "Non-Anginal Pain", 3: "Asymptomatic"}[x], help='Type of chest pain experienced')
+        fbs = st.radio('Fasting Blood Sugar > 120 mg/dl', options=[0, 1], format_func=lambda x: {0: "False", 1: "True"}[x], help='0: False, 1: True')
+        exang = st.radio('Exercise Induced Angina', options=[0, 1], format_func=lambda x: {0: "No", 1: "Yes"}[x], help='0: No, 1: Yes')
+        thal = st.selectbox('Thalassemia', options=[0, 1, 2, 3], format_func=lambda x: {0: "Normal", 1: "Fixed Defect", 2: "Reversible Defect", 3: "Other"}[x], help="Type of thalassemia")
 
     if st.button('Heart Disease Test Result'):
-
         user_input = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+        try:
+            heart_prediction = heart_disease_model.predict([user_input])
+            heart_diagnosis = 'The person is having heart disease' if heart_prediction[0] == 1 else 'The person does not have any heart disease'
+            st.success(heart_diagnosis)
+        except Exception as e:
+            st.error(f"Error in prediction: {e}")
 
-        user_input = [float(x) for x in user_input]
-
-        heart_prediction = heart_disease_model.predict([user_input])
-
-        if heart_prediction[0] == 1:
-            heart_diagnosis = 'The person is having heart disease'
-        else:
-            heart_diagnosis = 'The person does not have any heart disease'
-
-    st.success(heart_diagnosis)

a9b4321416af637687a4f92b581ecb59874e22ba
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2024-06-19 15:41:13 +0200

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -89,7 +89,7 @@ elif selected == 'Health Chatbot':
             name = feature_name[node]
             threshold = tree_.threshold[node]
             st.write(f"Do you have {name}?")
-            ans = st.text_area("Your Answer: ")
+            ans = st.text_input("Your Answer:", key=str(depth))
             if ans.lower() in ["yes", "y"]:
                 val = 1
             elif ans.lower() in ["no", "n"]:

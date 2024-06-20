453e93d968011dd784f8315e30104f0b6b681ddc
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2024-06-20 12:17:59 +0200

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -9,7 +9,12 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
-
+# Install transformers if not installed
+try:
+    import transformers
+except ImportError:
+    import subprocess
+    subprocess.check_call(["pip", "install", "transformers"])
 # Load necessary models and data
 working_dir = os.path.dirname(os.path.abspath(__file__))
 heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))

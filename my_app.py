296d493da5d2750befdadb02a46ce18c232f2516
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2024-06-20 12:25:04 +0200

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
-import subprocess  # Added import for subprocess
+import subprocess 
 import sys
 try:
     from transformers import BertTokenizer, BertForSequenceClassification
